cf0ddcd00aec7ca15b7ce94b929a806120ccca10
jiyasom <36759455+jiyasom@users.noreply.github.com>
2023-10-17 10:45:17 +0400

    styling the active link using classNames package

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,7 +1,9 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
-
+import classNames from "classnames";
+import { usePathname } from "next/navigation";
 const Navbar = () => {
   const links = [
     {
@@ -13,6 +15,7 @@ const Navbar = () => {
       href: "/issues",
     },
   ];
+  const currentPath = usePathname();
   return (
     <nav className="flex border-b space-x-6 px-5 h-14 items-center">
       <Link href="/" className=" ">
@@ -23,7 +26,11 @@ const Navbar = () => {
           <li key={link.name}>
             <Link
               href={link.href}
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={classNames({
+                "text-zinc-900": currentPath === link.href,
+                "text-zinc-500": currentPath !== link.href,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
             >
               {link.name}
             </Link>
---
+++ app/issues/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const issuesPage = () => {
+  return <div>issues Page</div>;
+};
+
+export default issuesPage;
