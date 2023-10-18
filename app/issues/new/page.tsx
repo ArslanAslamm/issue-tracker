f7aae7e82915a20ad3c91f0ccbf588ffa2d50ac7
jiyasom <36759455+jiyasom@users.noreply.github.com>
2023-10-19 02:11:11 +0400

    removed form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import IssueForm from "./IssueForm";
+// import IssueForm from "./IssueForm";
 
 const page = () => {
   return (
     <>
-      <IssueForm />
+      <div>wokring</div>
     </>
   );
 };
