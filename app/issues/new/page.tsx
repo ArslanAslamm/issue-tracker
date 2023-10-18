04b9c1ed9b50d62c0f2a65e5895fe81d3c2edb9e
jiyasom <36759455+jiyasom@users.noreply.github.com>
2023-10-18 04:08:00 +0400

    using SimpleMDE for textarea React

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 import React from "react";
 import { TextArea, TextField, Button } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 const page = () => {
   return (
     <div className="max-w-2xl p-5 space-y-3">
       <TextField.Root>
         <TextField.Input placeholder="Title" />
       </TextField.Root>
-      <TextArea placeholder="Description " />
+      <SimpleMDE placeholder="Description " />
       <Button>Add Issue</Button>
     </div>
   );
